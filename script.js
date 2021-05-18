7ff49849eb92cd40790bc5140f85df87649ac0a8
Arquimides <arquimides@Arquimidess-MacBook-Air.local>
2021-05-18 10:55:18 -0400

    flex flow issues

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-async function accessDatabase() {
+async function accessDatabase(name) {
   //const datatypes should be all-caps
   //test URL to return the card 'Lightning Bolt'
-  // const URL = `https://api.magicthegathering.io/v1/cards?name=Ghave,_Guru_of_Spores`
-  const URL = `https://api.magicthegathering.io/v1/cards?name=Goblin`
-  //to be assigned later based on what the user wants
-  const UserSearch = '';
+  const URL = `https://api.magicthegathering.io/v1/cards?name=${name}`
 
   //axios to access the URL above
   axios.get(URL)
@@ -45,56 +42,76 @@ async function accessDatabase() {
 
 }
 
-accessDatabase();
 
-const cardGallery = document.querySelector('#cardGallery');
+const searchButton = document.querySelector(`#searchButton`);
 
-function showToUser(returnedUniqueCards) {
-  const image = 'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg/revision/latest/scale-to-width-down/250?cb=20140813141013'
+searchButton.addEventListener('click', () => {
+  const searchInput = document.querySelector(`#searchBar`);
+  let searchValue = searchInput.value;
+  console.log(searchValue);
 
-  for (let i = 0; i < returnedUniqueCards.length; i++) {
-    const output =
-      `
-      <div class='card' style="background-image: url('${returnedUniqueCards[i].imageUrl}')"></div>
-      `
-    if (`${returnedUniqueCards[i].imageUrl}` == 'undefined') {
-      const imagelessOutput =
-      `
-      <div class='card' style="background-image: url('${image}')"></div>
-      `
-      // console.log('within imageless')
-      cardGallery.insertAdjacentHTML('afterbegin', imagelessOutput);
-      
-    } else {
-      cardGallery.insertAdjacentHTML('afterbegin', output);
-      console.log(i);
-      // console.log('within images')
-    }
+  accessDatabase(searchValue);
 
-  }
-}
+})
 
-//original function if errors result recomment this back in
+
+
+
+
+
+
+// const cardGallery = document.querySelector('#cardGallery');
+
 // function showToUser(returnedUniqueCards) {
 //   const image = 'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg/revision/latest/scale-to-width-down/250?cb=20140813141013'
+
 //   for (let i = 0; i < returnedUniqueCards.length; i++) {
 //     const output =
 //       `
-//     <p>${returnedUniqueCards[i].name}</p>
-//     <img src=${returnedUniqueCards[i].imageUrl}> 
-//     `
+//       <div class='card' style="background-image: url('${returnedUniqueCards[i].imageUrl}')"></div>
+//       `
 //     if (`${returnedUniqueCards[i].imageUrl}` == 'undefined') {
 //       const imagelessOutput =
 //       `
-//       <p>${returnedUniqueCards[i].name}</p>
-//       <img src=${image}>
+//       <div class='card' style="background-image: url('${image}')"></div>
 //       `
-//       document.querySelector(`#picture`).insertAdjacentHTML('afterbegin', imagelessOutput);
+//       // console.log('within imageless')
+//       cardGallery.insertAdjacentHTML('afterbegin', imagelessOutput);
       
 //     } else {
-//       document.querySelector(`#picture`).insertAdjacentHTML('afterbegin', output);
+//       cardGallery.insertAdjacentHTML('afterbegin', output);
+//       console.log(i);
+//       // console.log('within images')
 //     }
 
 //   }
 // }
+
+//original function if errors result recomment this back in
+function showToUser(returnedUniqueCards) {
+  const image = 'https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg/revision/latest/scale-to-width-down/250?cb=20140813141013'
+  for (let i = 0; i < returnedUniqueCards.length; i++) {
+    const output =
+      `
+    <div class='oCard'>
+      <p>${returnedUniqueCards[i].name}</p>
+      <img src=${returnedUniqueCards[i].imageUrl}>
+    </div>
+    `
+    if (`${returnedUniqueCards[i].imageUrl}` == 'undefined') {
+      const imagelessOutput =
+      `
+      <div class='oCard'>
+        <p>${returnedUniqueCards[i].name}</p>
+        <img class="noImage" src=${image}>
+      </div>
+      `
+      document.querySelector(`#picture`).insertAdjacentHTML('afterbegin', imagelessOutput);
+      
+    } else {
+      document.querySelector(`#picture`).insertAdjacentHTML('afterbegin', output);
+    }
+
+  }
+}
   
